dc09f2d4635e66c65290b10034bbb35d9ad026b0
Wayne Pan <waynepan@gmail.com>
2009-11-18 17:44:33 -0800

    adding documentation link and license

--- admob.js
+++ admob.js
@@ -1,8 +1,29 @@
 // AdMob Publisher Code
 // Language: webOS SDK
-// Version: 20091005
+// Version: 20091118
 // Copyright AdMob, Inc., All rights reserved
-// Documentation: http://developer.admob.com/wiki/WebOS
+// http://code.google.com/p/admob-webos/
+/**
+ * Copyright (c) 2009 AdMob, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 if (typeof AdMob == 'undefined') {
 	var AdMob = {};
